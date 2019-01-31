88885c4dc193c44b81adba276957bf15d8f2e069
Tatiana Veratti <tatianaveratti@gmail.com>
2019-01-30 23:38:01 -0600

    organize server routes/index

--- server/index.js
+++ server/index.js
@@ -3,20 +3,16 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const next = require('next');
 
+require('dotenv').config();
+
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 // Connect to MongoDb
 // =========================================
-const username = 'xx';
-const password = 'xx';
-const url =
-  `mongodb://${username}:${password}` +
-  'cluster0-shard-00-00-2omka.mongodb.net:27017,' +
-  'cluster0-shard-00-01-2omka.mongodb.net:27017,' +
-  'cluster0-shard-00-02-2omka.mongodb.net:27017/buffy' +
-  '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CONNECTION } = process.env;
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CONNECTION}`;
 
 mongoose.connect(
   url,
@@ -33,7 +29,7 @@ app
     server.use(bodyParser.urlencoded({ extended: true }));
 
     // API Routes
-    server.use('/api', require('./server/routes'));
+    server.use('/api', require('./routes'));
 
     server.get('*', (req, res) => {
       return handle(req, res);
